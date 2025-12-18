4ec1db2fcf782fc3e1bcfce559b1d309fc0b2e18
testtestingg <dridixhydra@gmail.com>
2025-12-18 22:34:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Gallery from './components/Gallery';
-import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Navigation from './components/Navigation';
 
@@ -26,7 +25,6 @@ function App() {
       <About />
       <Services />
       <Gallery />
-      <Testimonials />
       <Contact />
     </div>
   );
